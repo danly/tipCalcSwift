1038c1d63b46c4061ee6e60fcca62bf54fa71929
danlyg <gih.daniel@gmail.com>
2015-09-03 21:23:19 -0700

    created a tip calc in swift

--- tipCalc/ViewController.swift
+++ tipCalc/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var segControl: UISegmentedControl!
+    @IBOutlet weak var sliderControl: UISlider!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBAction func newTipPercentageSelected(sender: AnyObject) {
+        calcTip()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func billAmountAdded(sender: AnyObject) {
+        calcTip()
+        
     }
-
+    
+    func calcTip() {
+        var index = segControl.selectedSegmentIndex
+        var billAmount = (billTextField.text as NSString).doubleValue
+        var percentage = 0.0
+        switch index {
+        case 0:
+            percentage = 0.15
+        case 1:
+            percentage = 0.18
+        case 2:
+            percentage = 0.20
+        default:
+            println("nothing")
+        }
+        var tipAmount = billAmount * percentage
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", billAmount + tipAmount)
+    }
 
 }
 
